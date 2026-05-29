439744a3fe7dec95e4b71dc48065e9e6fdb5b288
pedrolanfranco-collab <pedro.lanfranco@gmail.com>
2026-05-29 20:02:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'monitor-infra-v10';
+const CACHE = 'monitor-infra-v11';
 const STATIC = [
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
